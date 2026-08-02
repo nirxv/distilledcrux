91f3380de804dbab39bd9add8f9881e764995d01
n3rxv <nirxv03@gmail.com>
2026-08-02 23:16:47 +0530

    fix: merge all subjects into allNotes for dynamic slug routing

--- lib/notes/index.ts
+++ lib/notes/index.ts
@@ -1,25 +1,25 @@
-import {
-  paper1Notes as sociologyP1,
-  paper2Notes as sociologyP2,
-  allNotes as sociologyAllNotes,
-  paper1Sections as sociologyP1Sections,
-  paper2Sections as sociologyP2Sections,
-} from './sociology';
-
-import {
-  paper1Notes as anthropologyP1,
-  paper2Notes as anthropologyP2,
-} from './anthropology';
+import { paper1Notes as socP1, paper2Notes as socP2, allNotes as socAll } from './sociology';
+import { paper1Notes as anthP1, paper2Notes as anthP2 } from './anthropology';
+import { paper1Notes as polP1, paper2Notes as polP2 } from './polsci';
+import { paper1Notes as geoP1, paper2Notes as geoP2 } from './geography';
+import { paper1Notes as paP1, paper2Notes as paP2 } from './pub-admin';
 
 export type { Note } from './sociology';
 
-export const paper1Notes = [...sociologyP1];
-export const paper2Notes = [...sociologyP2];
-export const allNotes = [...sociologyAllNotes, ...anthropologyP1, ...anthropologyP2];
+export const paper1Notes = [...socP1];
+export const paper2Notes = [...socP2];
+
+export const allNotes = [
+  ...socAll,
+  ...anthP1, ...anthP2,
+  ...polP1, ...polP2,
+  ...geoP1, ...geoP2,
+  ...paP1, ...paP2,
+];
 
 export function getNoteBySlug(slug: string) {
   return allNotes.find((n) => n.slug === slug);
 }
 
-export const paper1Sections = sociologyP1Sections;
-export const paper2Sections = sociologyP2Sections;
+export const paper1Sections = ['Fundamentals of Sociology', 'Social Structure', 'Social Stratification', 'Politics and Society', 'Religion and Society', 'Systems of Kinship', 'Social Change in Modern Society'];
+export const paper2Sections = ['Introducing Indian Society', 'Social Structure', 'Social Stratification', 'Challenges of Social Transformation'];
