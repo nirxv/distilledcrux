299ad055ab50b50b3e78f5ede87999f9571767f6
n3rxv <nirxv03@gmail.com>
2026-08-02 10:51:56 +0530

    fix: correct index.ts syntax (remove f-string escaping artifact)

--- lib/notes/index.ts
+++ lib/notes/index.ts
@@ -1,25 +1,25 @@
-import {{
+import {
   paper1Notes as sociologyP1,
   paper2Notes as sociologyP2,
   allNotes as sociologyAllNotes,
   paper1Sections as sociologyP1Sections,
   paper2Sections as sociologyP2Sections,
-}} from './sociology';
+} from './sociology';
 
-import {{
+import {
   paper1Notes as anthropologyP1,
   paper2Notes as anthropologyP2,
-}} from './anthropology';
+} from './anthropology';
 
-export type {{ Note }} from './sociology';
+export type { Note } from './sociology';
 
 export const paper1Notes = [...sociologyP1];
 export const paper2Notes = [...sociologyP2];
 export const allNotes = [...sociologyAllNotes, ...anthropologyP1, ...anthropologyP2];
 
-export function getNoteBySlug(slug: string) {{
+export function getNoteBySlug(slug: string) {
   return allNotes.find((n) => n.slug === slug);
-}}
+}
 
 export const paper1Sections = sociologyP1Sections;
 export const paper2Sections = sociologyP2Sections;
