75b7c0bb8d02a336c71c6935798a7e8ae81ce60f
nirxv <bniruv@gmail.com>
2026-08-06 18:21:38 +0530

    trigger vercel deployment

--- lib/noteContent/index.ts
+++ lib/noteContent/index.ts
@@ -1,9 +1,11 @@
+import { polsciNoteContent } from './polsci';
 import { sociologyNoteContent } from './sociology';
 import { anthropologyNoteContent } from './anthropology';
 
 export const noteContent: Record<string, string> = {
   ...sociologyNoteContent,
   ...anthropologyNoteContent,
+  ...polsciNoteContent,
 };
 
 export function getNoteContent(slug: string): string {
